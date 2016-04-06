f9ab3b8862e5c0367241539dd89db87b50d37cc4
cdelorme <cdelorme@gmail.com>
2016-04-05 22:20:20 -0400

    fix missing env vars at Dockerfile
    set defaults at Dockerfile instead of build script

--- rathena/add/docker/build.sh
+++ rathena/add/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 # simple steps to configure and build rathena
-./configure --enable-prere=${PRERE:-no} --enable-packetver=${PACKETVER:-20151029}
+./configure --enable-prere=$PRERE --enable-packetver=$PACKETVER
 make clean
 make server
